3acc01389b7446687efcabbbdcc45ae78b2baf49
dylinemf <dylinemely@yahoo.co.id>
2025-05-23 00:55:22 +0700

    secret updated

--- modular_project/modular_project/settings.py
+++ modular_project/modular_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e!=b#u9*$9+_=dc1kfahi0n-xcte!ywzspqse!v!jq7!wk%@#3'
+SECRET_KEY = os.environ.get("SECRET_KEY", "development-unsafe-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
